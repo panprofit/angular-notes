61f4d69b3348bf91d2fdfdd01ec6ee4e2ddc6de3
panprofit <panprofit@gmail.com>
2019-04-26 21:13:32 +0300

    first commit

--- src/app/shared/get-tags.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { GetTagsPipe } from './get-tags.pipe';
-
-describe('GetTagsPipe', () => {
-  it('create an instance', () => {
-    const pipe = new GetTagsPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- src/app/shared/note-tag.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { NoteTagPipe } from './note-tag.pipe';
-
-describe('NoteTagPipe', () => {
-  it('create an instance', () => {
-    const pipe = new NoteTagPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
